bef2ef44cd1825b479c90e5fdd0866b3e0fa1ed1
Kokila Widyaratna <kokilapc@gmail.com>
2019-05-16 19:19:07 +0530

    support for paragraphs

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import sys
 import os
 
 from pre_process import normalize_string
+from utils import tokenize_sinhala_text
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -17,7 +18,7 @@ global corrector, model, params
 # Building and loading the keras model, params file contains the encoding/decoding dictionaries.
 corrector, model, params = init()
 
-regexp = re.compile(r'[^\u0D80-\u0DFF.!?\s\u200d]')
+regexp = re.compile(r'[^\u0D80-\u0DFF.!?,\s\u200d]')
 
 
 def valid_sinhala_sentence(sentence):
@@ -35,15 +36,18 @@ def get_corrections(sentence, useBeamSearch=False, beamSize=3):
     normalized_sentence = normalize_string(sentence)
     normalized_sentence = normalized_sentence.strip()
     corrections = {}
+    corrections["sentence"] = normalized_sentence
     if useBeamSearch:
         corrections['results'] = corrector.get_beam_search_corrections(
             normalized_sentence, beamSize)
+        corrections['isCorrect'] = corrections['results'][0]["suggestion"] == normalized_sentence
     else:
         corrections['results'] = [{
-            "sequence": corrector.get_greedy_search_correction(
+            "suggestion": corrector.get_greedy_search_correction(
                 normalized_sentence),
             "probability": None
         }]
+        corrections['isCorrect'] = corrections['results'][0]["suggestion"] == normalized_sentence
 
     return corrections
 
@@ -76,12 +80,43 @@ def correct():
         abort(400, "Invalid JSON request")
 
 
+@app.route('/correct/paragraph', methods=['POST'])
+@cross_origin()
+def correct_paragraph():
+    if request.is_json:
+        request_data = request.get_json()
+        print(request_data)
+
+        if 'text' in request_data.keys() and valid_sinhala_sentence(request_data['text']):
+            text = request_data['text']
+            useBeamSearch = request_data['useBeamSearch'] if 'useBeamSearch' in request_data.keys(
+            ) else False
+            sentences = tokenize_sinhala_text(text)
+
+            sentence_corrections = []
+
+            for sentence in sentences:
+                corrections = get_corrections(
+                    sentence, useBeamSearch=useBeamSearch)
+                sentence_corrections.append(corrections)
+
+            evaluation = {'useBeamSearch': useBeamSearch, "sentences": sentence_corrections}
+
+            return jsonify(evaluation)
+        else:
+            abort(422, "Unsupported argument. Invalid Sinhala sentence type.")
+
+        return jsonify(request_data)
+    else:
+        abort(400, "Invalid JSON request")
+
+
 if __name__ == "__main__":
     # decide what port to run the app in
     # port = int(os.environ.get('PORT', 5000))
     port = int(os.environ.get('PORT', 80))
     # run the app locally on the givn port
-    app.run(host='0.0.0.0', port=port)
+    # app.run(host='0.0.0.0', port=port)
     # optional if we want to run in debugging mode
     # app.run(debug=True)
-    # app.run()
+    app.run()
---
+++ utils/utils.py
@@ -0,0 +1,87 @@
+import re
+
+sinhala_punctuation_marks = [".", ",", "!", "?"]
+
+short_forms = {
+    "ඒ.": "aa",
+    "බී.": "bb",
+    "සී.": "cc",
+    "ඩී.": "dd",
+    "ඊ.": "ee",
+    "එෆ්.": "ff",
+    "ජී.": "gg",
+    "එච්.": "hh",
+    "අයි.": "ii",
+    "ජේ.": "jj",
+    "කේ.": "kk",
+    "එල්.": "ll",
+    "එම්.": "mm",
+    "එන්.": "nn",
+    "ඕ.": "oo",
+    "පී.": "pp",
+    "කිව්.": "qq",
+    "ආර්.": "rr",
+    "එස්.": "ss",
+    "ටී.": "tt",
+    "යූ.": "uu",
+    "ඩබ්.": "WW",
+    "ඩබ්ලිව්.": "ww",
+    "එක්ස්.": "xx",
+    "වයි.": "yy",
+    "ඉසෙඩ්.": "zz",
+    "පෙ.": "pe",
+    "ව.": "wa",
+    "ප.": "pa",
+    "රු.": "ru",
+    "0.": "0dot", "1.": "1dot", "2.": "2dot", "3.": "3dot", "4.": "4dot", "5.": "5dot",
+    "6.": "6dot", "7.": "7dot", "8.": "8dot", "9.": "dot"
+}
+
+short_form_identifier = "\u0D80"
+
+
+def encode_short_form_with_escape_values(text):
+    encode_text = text
+    for short_form, escape_form in short_forms.items():
+        encode_text = encode_text.replace(short_form, escape_form)
+
+    return encode_text
+
+
+def decode_short_form_with_escape_values(text):
+    decode_text = text
+
+    for short_form, escape_form in short_forms.items():
+        decode_text = decode_text.replace(escape_form, short_form)
+
+    return decode_text
+
+
+def tokenize_sinhala_text(text):
+    escape_encoded_text = encode_short_form_with_escape_values(text)
+
+    escape_encoded_text = re.sub(r"([.!?])", r"\1\t", escape_encoded_text)
+    sentences = escape_encoded_text.split("\t")
+    sentences.remove('')
+
+    decoded_sentences = []
+
+    for sentence in sentences:
+        decoded_sentence = decode_short_form_with_escape_values(sentence)
+        decoded_sentences.append(decoded_sentence)
+
+    return decoded_sentences
+
+
+def split(txt, seps):
+    default_sep = seps[0]
+
+    # we skip seps[0] because that's the default seperator
+    for sep in seps[1:]:
+        txt = txt.replace(sep, default_sep)
+    return [i.strip() for i in txt.split(default_sep)]
+
+
+if __name__ == "__main__":
+    text = "මම පෙ.ව. 10ට එතැනට පැමිණියෙමි. මම පෙ.ව. 10ට එතැනට පැමිණියෙමි. මම පෙ.ව. 10ට එතැනට පැමිණියෙමි."
+    print(tokenize_sinhala_text(text))
